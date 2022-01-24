37d0e425bb26e38ae2364fe200bf7eba5f84fdad
YESENIA2012 <gyesenia173@gmail.com>
2022-01-24 12:50:03 -0500

    Solve my first

---
+++ firstChallengeHackerRank.js
@@ -0,0 +1,11 @@
+/*Complete the function solveMeFirst to compute the sum of two integers.*/
+var a = 5;
+var b = 10;
+
+function solveMeFirst(a, b) {
+  var sum = a + b;
+  return sum;
+}
+
+var sum = solveMeFirst(a, b);
+console.log(sum);
