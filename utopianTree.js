49a914c95cfe8d130b3e5a6818d578f10ed98625
YESENIA2012 <gyesenia173@gmail.com>
2022-07-11 12:24:04 -0500

    Solve utopian tree

---
+++ utopianTree.js
@@ -0,0 +1,23 @@
+/*The Utopian Tree goes through 2 cycles of growth every year. 
+Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. 
+How tall will the tree be after n growth cycles? */
+
+let n = 4;
+
+function utopianTree(n) {
+  let height = 1;
+
+  for (let counter = 1; counter <= n; counter++) {
+    if (counter % 2 == 1) {
+      height *= 2;
+    } else {
+      height += 1;
+    }
+  }
+  return height;
+}
+
+let resultado = utopianTree(n);
+
+console.log("Height", resultado);
